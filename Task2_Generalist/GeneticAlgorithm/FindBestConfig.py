2a90cfc0b0144f8ac0fc93ae770d5a1ee0351169
riverawesome <r.vaudrin@hotmail.com>
2023-10-19 13:51:13 +0200

    is this it??????

--- Task2_Generalist/GeneticAlgorithm/FindBestConfig.py
+++ Task2_Generalist/GeneticAlgorithm/FindBestConfig.py
@@ -3,7 +3,7 @@ import pandas as pd
 test_statistic = 'max_gain'
 
 # Load the data from data.txt into a Pandas DataFrame
-df = pd.read_csv('FINALParameterTuning/results.txt', delim_whitespace=True, header=0, names=["parameter_comb", "trial", "mean_fitness", "max_fitness", "mean_gain", "max_gain"])
+df = pd.read_csv('Results/FINALParameterTuning/results.txt', delim_whitespace=True, header=0, names=["parameter_comb", "trial", "mean_fitness", "max_fitness", "mean_gain", "max_gain"])
 print(df)
 # Group the data by 'parameter_comb' and calculate the mean of 'mean_gain' for each group
 mean_stat_by_comb = df.groupby('parameter_comb')[test_statistic].mean()
